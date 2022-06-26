bc74e33e964f30690890a1b057e70d9102d7329e
omid salangi <omidsalangi@outlook.com>
2022-06-27 01:29:55 +0430

    create details of clean architecture

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Application.Model;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
